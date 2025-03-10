5616542e232ddf201d209defb4b73c33d46566ae
Alexey Maksimov <MaksimovAM@pay-trans.ru>
2025-03-10 09:21:23 +0300

    imporove build

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ root_dir=$(dirname $(readlink -f $0))
 
 old_dir=$(pwd)
 cd $root_dir
-dpkg-buildpackage -b --no-sign -rfakeroot && \
+dpkg-buildpackage -b --no-sign && \
     find $root_dir/.. -maxdepth 1 -name '*.deb' -exec mv {} $root_dir \;
 
 
